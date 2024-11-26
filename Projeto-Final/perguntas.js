12dde47c40d933300399f3bf260ee19de1088cfd
Biancaf-Gomes <biancaferreiragomes216@gmail.com>
2024-11-26 08:44:46 -0300

    Add files via upload

---
+++ Projeto-Final/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação',
+)
+
+criaCartao(
+    'Geografia',
+    pergunta,
+    resposta,
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
