9a35e97179d8a8315de9a5a91b402798f6953328
Biancaf-Gomes <biancaferreiragomes216@gmail.com>
2024-11-26 22:18:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'programação',
+    'Oque é uma função',
+    'Uma funçao é um bloco de  código que executa alguma tarefa'
+)
